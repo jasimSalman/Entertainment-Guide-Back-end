713440f13feafb2798216decb2e5e67cb890869d
jasimSalman <jassimmo7amed+1@gmail.com>
2024-06-12 09:24:15 +0300

    edite some errors

--- controllers/places.js
+++ controllers/places.js
@@ -1,16 +1,25 @@
-const Place = require("../models/place")
-const Category = require("../models/category")
+const Place = require('../models/place')
+const Category = require('../models/category')
 
-async function index(req, res) {
+const index = async (req, res) => {
   // const paramId = req.params.id
   // const categories = await Category.findById(paramId)
   const categories = await Category.findById()
   // const places = await Place.find({ placeType: categories.categoryName })
   const places = await Place.find()
-  res.json(places)
+  res.send(places)
   //http://localhost:3001/places
+}
+
+const show = async (req, res) => {
+  const placeId = req.params.placeId
+  console.log(placeId)
+  const places = await Place.findById(placeId)
+  console.log(places)
+  res.send(places)
 }
 
 module.exports = {
   index,
+  show
 }
--- routes/places.js
+++ routes/places.js
@@ -1,13 +1,13 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
 
-const placesCtrl = require("../controllers/places")
+const placesCtrl = require('../controllers/places')
 
-router.get("/", placesCtrl.index)
+router.get('/', placesCtrl.index)
 
 // router.get('/:placeId/reviews', placesCtrl.review)
 
-// router.get('/:placeId', placesCtrl.show)
+router.get('/:placeId', placesCtrl.show)
 
 // router.post('/:placeId/reviews/:userId', placesCtrl.addReview)
 
