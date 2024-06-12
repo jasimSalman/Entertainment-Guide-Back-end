e00d4d4919a766303a4a32de9d01cfb2c21ac326
jasimSalman <jassimmo7amed+1@gmail.com>
2024-06-12 22:35:28 +0300

    create delete review function and fix the add review function

--- controllers/lists.js
+++ controllers/lists.js
@@ -1,32 +1,29 @@
 const User = require('../models/user')
 const Place = require('../models/place')
 
+//This function will show all the places in the user's favorite list.
 const index = async (req, res) => {
-  console.log(`The request == >${req}`)
+  // console.log(`The request == >${req}`)
   const userId = req.params.userId
-  console.log(`User id ==> ${userId}`)
-
+  // console.log(`User id ==> ${userId}`)
   const user = await User.findById(userId)
-  console.log(`User from the DB == >${user}`)
-
+  // console.log(`User from the DB == >${user}`)
   const lists = user.userList
-
   const places = await Place.find({ _id: { $in: lists } })
-
   res.send(places)
 }
 
+//This function is responsible for adding new places to the favorite list.
 const add = async (req, res) => {
   try {
     const palceId = req.params.palceId
 
-    const userId = '66689455cedf867f427f7325'
-
+    const userId = '66689455cedf867f427f7325' //remove it later.
     const user = await User.findById(userId)
-    console.log(`User id from the request  ${user}`)
+    // console.log(`User id from the request  ${user}`)
     user.userList.push(palceId)
     await user.save()
-    res.send('Saves succefully ')
+    // res.send('Saves succefully ')
   } catch (e) {
     console.error(e)
   }
