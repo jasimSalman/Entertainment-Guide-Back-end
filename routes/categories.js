794fe6b62227e570abef37d71debb7057705b19f
zainabtalaq <zainab.altallaq@gmail.com>
2024-06-16 16:39:01 +0300

    categoryId

--- routes/categories.js
+++ routes/categories.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const categoriesCtrl = require("../controllers/categories")
 
 router.get("/", categoriesCtrl.index)
-router.get("/:id/places", categoriesCtrl.show)
+router.get("/:categoryId/places", categoriesCtrl.show)
 
 module.exports = router
